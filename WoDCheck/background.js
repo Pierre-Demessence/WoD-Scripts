15ec48fff62706aac9276f603d5b1e52236e1d4a
Pierre Demessence <pierredemessence@gmail.com>
2014-09-25 11:54:33 +0200

    Update of the extension : * State is now a bitfield to manage several states at once. * A notification is now shown when the state change with the list of all the different things to see in WoD. * Code is uglier than before.

--- WoDCheck/background.js
+++ WoDCheck/background.js
@@ -1,49 +1,102 @@
-var DSC		= -1;
-var NONE	= 0;
-var MSG		= 1;
-var GRP		= 2;
-var CLN		= 3;
+var DSC		= parseInt('0001', 2);
+var MSG		= parseInt('0010', 2);
+var GRP		= parseInt('0100', 2);
+var CLN		= parseInt('1000', 2);
 
-var STATE = NONE;
+var STATE = 0;
+var STATE_OLD = 0;
 
-function setBadge() {
+var MSG_NB = 0;
+
+var URL = "http://ezantoh.world-of-dungeons.fr/";
+
+function notify() {
 	var text = "";
-	switch (STATE) {
-		case DSC:
-			text = "???"; break;
-		case MSG:
-			text = "MSG"; break;
-		case GRP:
-			text = "GRP"; break;
-		case CLN:
-			text = "CLN"; break;
-		default:
-			text = ""; break;
+	var list = [];
+	if (STATE & DSC)
+	{
+		if (text == "") text = "???";
+		list.push({title:"*", message:"Vous n'êtes pas connecté."});
 	}
+	if (STATE & MSG)
+	{
+		if (text == "") text = ""+MSG_NB;
+		list.push({title:"*", message:"Vous avez "+MSG_NB+" nouveaux messages."});
+	}
+	if (STATE & GRP)
+	{
+		if (text == "") text = "GRP";
+		list.push({title:"*", message:"Nouveaux messages de groupe."});
+	}
+	if (STATE & CLN)
+	{
+		if (text == "") text = "CLN";
+		list.push({title:"*", message:"Nouveaus messages de clan."});
+	}
+		
 	chrome.browserAction.setBadgeText({text:text});
+	chrome.notifications.onClicked.addListener(function (notificationId) {
+		chrome.tabs.create({'url': URL}, function(tab) {});
+	});
+	if (STATE != STATE_OLD)
+	{
+		chrome.notifications.create("42", {type:"list", iconUrl:"WOD.png", title:"Notifications", message:"", items:list}, function(notificationId) {});
+		chrome.notifications.clear("42", function (wasCleared) {});
+		STATE_OLD = STATE;
+	}
+}
+function checkMessages() {
+	MSG_NB = 0;
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			var html = xhr.responseText;
+			var matches = html.match(/\(\d\)" class="folder/ig);
+			for (i in matches)
+				MSG_NB += parseInt(matches[i].substr(1));
+		}
+	}
+	xhr.open("GET", 'http://ezantoh.world-of-dungeons.fr/wod/spiel/pm/pm.php', false);
+	xhr.send();
 }
 
-function check() {
-	chrome.browserAction.setBadgeText({text:"..."});
+function checkStates() {
+	STATE = 0;
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			var html = xhr.responseText;
 			if (html.search("Connexion") != -1)
-				STATE = DSC;
-			else if (html.search("/wod/css//skins/skin-8/images/icons/mail_new") != -1)
-				STATE = MSG;
-			else if (html.search("/wod/css//skins/skin-8/images/icons/forum_group_new") != -1)
-				STATE = GRP;
-			else if (html.search("/wod/css//skins/skin-8/images/icons/forum_clan_new") != -1)
-				STATE = CLN;
-			else
-				STATE = NONE;
-			setBadge();
+				STATE |= DSC;
+			if (html.search("/wod/css//skins/skin-8/images/icons/mail_new") != -1)
+				STATE |= MSG;
+			if (html.search("/wod/css//skins/skin-8/images/icons/forum_group_new") != -1)
+				STATE |= GRP;
+			if (html.search("/wod/css//skins/skin-8/images/icons/forum_clan_new") != -1)
+				STATE |= CLN;
 		}
 	}
-	xhr.open("GET", 'http://ezantoh.world-of-dungeons.fr/', true);
+	xhr.open("GET", 'http://ezantoh.world-of-dungeons.fr/', false);
 	xhr.send();
+}
+
+function setUrl()
+{
+	URL = "http://ezantoh.world-of-dungeons.fr/";
+	if (STATE & MSG)
+		URL = "http://ezantoh.world-of-dungeons.fr/wod/spiel/pm/pm.php";
+	else if (STATE & GRP)
+		URL = "http://ezantoh.world-of-dungeons.fr/wod/spiel/forum/?board=gruppe";
+	else if (STATE & CLN)
+		URL = "http://ezantoh.world-of-dungeons.fr/wod/spiel/forum/?board=clan";
+}
+
+function check() {
+	chrome.browserAction.setBadgeText({text:"..."});
+	checkStates();
+	checkMessages();
+	setUrl();
+	notify();
 }
 
 chrome.alarms.create("check", {
@@ -56,16 +109,5 @@ chrome.alarms.onAlarm.addListener(function(alarm){
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var url;
-	switch (STATE) {
-		case MSG:
-			url = "http://ezantoh.world-of-dungeons.fr/wod/spiel/pm/pm.php"; break;
-		case GRP:
-			url = "http://ezantoh.world-of-dungeons.fr/wod/spiel/forum/?board=gruppe"; break;
-		case CLN:
-			url = "http://ezantoh.world-of-dungeons.fr/wod/spiel/forum/?board=clan"; break;
-		default:
-			url = "http://ezantoh.world-of-dungeons.fr/"; break;
-	}
-	chrome.tabs.create({'url': url}, function(tab) {});
+	chrome.tabs.create({'url': URL}, function(tab) {});
 });
